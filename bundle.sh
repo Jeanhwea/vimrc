4c565707520dc7f65f6786825c1a6becd05e69d8
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-07-28 21:29:32 +0800

    shorten command name

--- bundle.sh
+++ bundle.sh
@@ -4,8 +4,8 @@ BBLUE="\033[01;34m"
 BGREEN="\033[01;32m"
 DEFAULT="\033[00m"
 
-# __clone repo name
-__clone() {
+# _clone repo name
+_clone() {
   local repo=$1
   local name=$2
   local des="$HOME/.vim/bundle/$name"
@@ -22,41 +22,40 @@ __clone() {
   fi
 }
 
-__clone Jeanhwea/vim-matchit matchit
-__clone Jeanhwea/vim-ultisnips-snippets ultisnips-snippets
-# __clone Jeanhwea/vim-viz syntax-viz
-__clone SirVer/ultisnips ultisnips
-__clone airblade/vim-gitgutter gitgutter
-__clone altercation/vim-colors-solarized colorscheme-solarized
-__clone bps/vim-textobj-python textobj-python
-__clone ctrlpvim/ctrlp.vim ctrlp
-__clone fs111/pydoc.vim pydoc
-__clone godlygeek/tabular tabular
-__clone hdima/python-syntax syntax-python
-__clone jceb/vim-textobj-uri textobj-uri
-__clone kana/vim-textobj-user textobj-user
-__clone majutsushi/tagbar tagbar
-__clone mileszs/ack.vim ack
-__clone plasticboy/vim-markdown syntax-markdown
-__clone rking/ag.vim ag
-__clone scrooloose/syntastic syntastic
-__clone sgur/vim-textobj-parameter textobj-parameter
-__clone sjl/gundo.vim gundo
-__clone tacahiroy/ctrlp-funky ctrlp-funky
-__clone tommcdo/vim-exchange exchange
-__clone tpope/vim-abolish abolish
-__clone tpope/vim-commentary commentary
-__clone tpope/vim-dispatch dispatch
-__clone tpope/vim-endwise endwise
-__clone tpope/vim-eunuch eunuch
-__clone tpope/vim-fugitive fugitive
-__clone tpope/vim-pathogen pathogen
-__clone tpope/vim-rails rails
-__clone tpope/vim-repeat repeat
-__clone tpope/vim-rhubarb rhubarb
-__clone tpope/vim-scriptease scriptease
-__clone tpope/vim-sensible sensible
-__clone tpope/vim-surround surround
-__clone tpope/vim-unimpaired unimpaired
-__clone tpope/vim-dadbod dasbod
-
+_clone Jeanhwea/vim-matchit matchit
+_clone Jeanhwea/vim-ultisnips-snippets ultisnips-snippets
+# _clone Jeanhwea/vim-viz syntax-viz
+_clone SirVer/ultisnips ultisnips
+_clone airblade/vim-gitgutter gitgutter
+_clone altercation/vim-colors-solarized colorscheme-solarized
+_clone bps/vim-textobj-python textobj-python
+_clone ctrlpvim/ctrlp.vim ctrlp
+_clone fs111/pydoc.vim pydoc
+_clone godlygeek/tabular tabular
+_clone hdima/python-syntax syntax-python
+_clone jceb/vim-textobj-uri textobj-uri
+_clone kana/vim-textobj-user textobj-user
+_clone majutsushi/tagbar tagbar
+_clone mileszs/ack.vim ack
+_clone plasticboy/vim-markdown syntax-markdown
+_clone rking/ag.vim ag
+_clone scrooloose/syntastic syntastic
+_clone sgur/vim-textobj-parameter textobj-parameter
+_clone sjl/gundo.vim gundo
+_clone tacahiroy/ctrlp-funky ctrlp-funky
+_clone tommcdo/vim-exchange exchange
+_clone tpope/vim-abolish abolish
+_clone tpope/vim-commentary commentary
+_clone tpope/vim-dispatch dispatch
+_clone tpope/vim-endwise endwise
+_clone tpope/vim-eunuch eunuch
+_clone tpope/vim-fugitive fugitive
+_clone tpope/vim-pathogen pathogen
+_clone tpope/vim-rails rails
+_clone tpope/vim-repeat repeat
+_clone tpope/vim-rhubarb rhubarb
+_clone tpope/vim-scriptease scriptease
+_clone tpope/vim-sensible sensible
+_clone tpope/vim-surround surround
+_clone tpope/vim-unimpaired unimpaired
+_clone tpope/vim-dadbod dasbod
